ed98954c028db0ed5cfbbb734a60763025c07e71
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-09-27 19:02:00 +0530

    UC4 - If tie then continues till the difference of minimum 2 points is achieved

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,16 +2,16 @@
 
 head=0
 tail=0
+
 for (( i=1; i<=50; i++ ))
 do
-	x=`echo $(($RANDOM%2))`
-	if [ $x -eq 1 ]
+	random=`echo $(($RANDOM%2))`
+	if [ $random -eq 0 ]
 	then
 		head=$(( $head + 1 ))
 	else
 		tail=$(( $tail + 1 ))
 	fi
-
 	if [[ $head -eq 21 || $tail -eq 21 ]]
 	then
 		y=$(( $head - $tail ))
@@ -20,7 +20,7 @@ do
 		then
 			break
 		else
-			x=`echo $(( $RANDOM%2 ))`
+			x=`echo $(($RANDOM%2))`
 			if [[ $x -eq 0 ]]
 		        then
                 		head=$(( $head + 1 ))
@@ -29,8 +29,27 @@ do
         		fi
 			if [[ $head -eq $tail ]]
 			then
+				tie=0
 				echo "Tie!!! Head=$head Tail=$tail"
-				exit
+				while [ $tie -eq 0 ]
+				do
+					x=`echo $(( $RANDOM%2 ))`
+				        if [ $x -eq 0 ]
+        				then
+                				head=$(( $head + 1 ))
+        				else
+                				tail=$(( $tail + 1 ))
+        				fi
+					y=$(( $head - $tail ))
+					z=$(( $tail - $head ))
+					if [[ $y -ge 2 || $z -ge 2 ]]
+					then
+						tie=1
+					else
+						tie=0
+					fi
+				done
+				break
 			elif [[ $head > $tail ]]
 			then
 				head=$(( $head - 1 ))
